c9f73313979c853c0d3e06b040c0ad83d8bd3785
Niki <nicole.bellert@gmail.com>
2016-02-01 13:51:21 +0100

    Changed the DB URL to use EC2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+	//url : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu'
+    url : 'mongodb://nikibelle:schnuppi537@ec2-54-216-120-165.eu-west-1.compute.amazonaws.com:27017/dummyDB'
 }
